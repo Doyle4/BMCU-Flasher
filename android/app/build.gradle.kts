0840c35c0cbe2d3201db28d64c871e05a4e6b97d
PJarczak <paweljarczak@int.pl>
2026-03-05 13:53:01 +0100

    avoid kotlin extension clash

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
   id("com.android.application")
-  id("org.jetbrains.kotlin.android")
+  id("org.jetbrains.kotlin.android") apply false
+}
+
+if (extensions.findByName("kotlin") == null) {
+  apply(plugin = "org.jetbrains.kotlin.android")
 }
 
 android {
@@ -29,10 +33,6 @@ android {
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
-  }
-
-  kotlinOptions {
-    jvmTarget = "17"
   }
 
   buildFeatures {
